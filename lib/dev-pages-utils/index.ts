cfa62cc6e6b5e2cba66eaea236b8c12198e9567d
Amanuel Sisay <amanabiy@amazon.de>
2026-01-09 17:21:18 +0100

    feat: add dev page utils

---
+++ lib/dev-pages-utils/index.ts
@@ -0,0 +1,7 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { PermutationsView, PermutationsViewProps } from "./permutations-view";
+import { createPermutations, ComponentPermutations } from "./permutations";
+
+export { createPermutations, PermutationsView, ComponentPermutations, PermutationsViewProps };
