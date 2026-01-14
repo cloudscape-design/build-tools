e86481c292652461378ce9b06a160e477be8a9da
Amanuel Sisay <amanabiy@amazon.de>
2026-01-14 20:30:51 +0100

    fix: add type to allow compatibility with --isolatedModules flag

--- lib/dev-pages-utils/index.ts
+++ lib/dev-pages-utils/index.ts
@@ -4,4 +4,4 @@
 import { PermutationsView, PermutationsViewProps } from "./permutations-view";
 import { createPermutations, ComponentPermutations } from "./permutations";
 
-export { createPermutations, PermutationsView, ComponentPermutations, PermutationsViewProps };
+export { createPermutations, PermutationsView, type ComponentPermutations, type PermutationsViewProps };
