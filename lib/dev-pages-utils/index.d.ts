7ce15ba0809c95dc622e052ee4d5de282e603ffe
Amanuel Sisay <amanabiy@amazon.de>
2026-01-16 15:13:41 +0100

    chore: auto update built files

--- lib/dev-pages-utils/index.d.ts
+++ lib/dev-pages-utils/index.d.ts
@@ -1,3 +1,3 @@
-import { PermutationsView, PermutationsViewProps } from "./permutations-view";
-import { createPermutations, ComponentPermutations } from "./permutations";
+import { PermutationsView, PermutationsViewProps } from "./permutations-view.js";
+import { createPermutations, ComponentPermutations } from "./permutations.js";
 export { createPermutations, PermutationsView, type ComponentPermutations, type PermutationsViewProps };
