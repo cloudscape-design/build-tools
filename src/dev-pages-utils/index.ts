977902a538236ead2ed839ea3a7e11a8ff2fc3b3
Amanuel Sisay <amanabiy@amazon.de>
2026-01-16 15:08:32 +0100

    fix: compatible

--- src/dev-pages-utils/index.ts
+++ src/dev-pages-utils/index.ts
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { PermutationsView, PermutationsViewProps } from "./permutations-view";
-import { createPermutations, ComponentPermutations } from "./permutations";
+import { PermutationsView, PermutationsViewProps } from "./permutations-view.js";
+import { createPermutations, ComponentPermutations } from "./permutations.js";
 
 export { createPermutations, PermutationsView, type ComponentPermutations, type PermutationsViewProps };
