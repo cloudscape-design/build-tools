b70cb896e5c89793ecf8d48e3c94dfd2889a46a7
Andrei Zhaleznichenka <andrei.zhaleznichenka@gmail.com>
2026-01-27 10:38:15 +0100

    chore: Refactors build and adds shared prepare package lock script (#29)

--- src/stylelint/index.js
+++
@@ -1,8 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-import licenseHeaders from "./license-headers.js";
-import noImplicitDescendant from "./no-implicit-descendant.js";
-import noMotionOutsideOfMixin from "./no-motion-outside-of-mixin.js";
-import zIndexValueConstraint from "./z-index-value-constraint.js";
-
-export default [noImplicitDescendant, noMotionOutsideOfMixin, licenseHeaders, zIndexValueConstraint];
