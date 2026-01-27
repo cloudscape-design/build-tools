fea9944569c8520d394d6f475243c452f5f04fbe
Amanuel Sisay <amanabiy@amazon.de>
2026-01-27 18:31:55 +0100

    chore: remove compiled code and add typescript files from src

--- lib/dev-pages-utils/permutations-view.d.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-export interface PermutationsViewProps<T> {
-    permutations: ReadonlyArray<T>;
-    render: (_props: T, _index?: number) => React.ReactElement;
-    direction?: "vertical" | "horizontal";
-}
-export declare function PermutationsView<T>({ permutations, render, direction }: PermutationsViewProps<T>): React.JSX.Element;
--- lib/dev-pages-utils/permutations.d.ts
+++
@@ -1,4 +0,0 @@
-export type ComponentPermutations<Props> = {
-    [prop in keyof Props]: ReadonlyArray<Props[prop]>;
-};
-export declare function createPermutations<Props>(permutations: Array<ComponentPermutations<Props>>): Props[];
