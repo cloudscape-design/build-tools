60001cc6399b58ebd3b827f0c64a4bd10ea48482
Amanuel Abiy <amanabiy@amazon.de>
2026-01-28 14:41:21 +0100

    refactor: Simplify dev-page-utils structure and remove automatic build on push (#30)

--- lib/dev-pages-utils/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { PermutationsView, PermutationsViewProps } from "./permutations-view.js";
-import { createPermutations, ComponentPermutations } from "./permutations.js";
-export { createPermutations, PermutationsView, type ComponentPermutations, type PermutationsViewProps };
