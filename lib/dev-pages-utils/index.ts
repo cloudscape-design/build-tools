bcb730daec375ae6d7fd6a2fd5935404136e6184
Maximilian Schoell <mxschll@users.noreply.github.com>
2026-07-06 10:30:39 +0200

    feat: add AppModesProvider

--- lib/dev-pages-utils/index.ts
+++ lib/dev-pages-utils/index.ts
@@ -4,13 +4,6 @@
 import { PermutationsView, PermutationsViewProps } from "./permutations-view.js";
 import { createPermutations, ComponentPermutations } from "./permutations.js";
 
-export {
-  type AppUrlParams,
-  type AppContextType,
-  appModesDefaults,
-  applyAppModes,
-  formatAppModes,
-  parseAppModes,
-  updateAppModes,
-} from "./app-modes.js";
+export { type AppUrlParams } from "./app-modes.js";
+export { AppModesProvider, useAppModes } from "./app-modes-provider.js";
 export { createPermutations, PermutationsView, type ComponentPermutations, type PermutationsViewProps };
